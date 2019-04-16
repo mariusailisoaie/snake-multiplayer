6ba94fb54c375e494d017ef9cffbd5ee59374964
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-16 21:07:02 +0200

    Remove get route and serve static folder

--- server/app.js
+++ server/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('OK');
-});
+app.use(express.static('public'));
 
 const PORT = 3000;
 const server = app.listen(PORT || process.env.PORT, () => {
