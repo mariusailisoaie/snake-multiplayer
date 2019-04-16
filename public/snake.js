94574f10b217be441009d067aad80facb03c191a
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-16 22:37:33 +0200

    Add snake.js

---
+++ public/snake.js
@@ -0,0 +1,11 @@
+const snakeParts = [
+  { x: 180, y: 200 },
+  { x: 160, y: 200 },
+  { x: 140, y: 200 },
+]
+
+class Snake {
+  constructor(snakeParts) {
+    this.snakeParts = snakeParts;
+  }
+}
