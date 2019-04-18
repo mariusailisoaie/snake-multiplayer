e69b25b82044380b124a265b53377cb43ac3ef3b
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-18 15:19:01 +0200

    Add socket.io in server and wait for connections

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
@@ -7,4 +8,10 @@ app.use(express.static('../public'));
 const PORT = 3000;
 const server = app.listen(PORT || process.env.PORT, () => {
   console.log(`server running on port ${PORT}`);
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('connection made', socket.id);
 });
