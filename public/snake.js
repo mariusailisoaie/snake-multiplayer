92e163e06b4dbecb71ebbf1c379699147673fc33
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-20 23:56:31 +0200

    Move socket snake logic in drawSnake

--- public/snake.js
+++ public/snake.js
@@ -19,6 +19,17 @@ class Snake {
     ctx.fillStyle = headColor;
     ctx.fillRect(this.snakeParts[0].x, this.snakeParts[0].y, 20, 20);
     ctx.strokeRect(this.snakeParts[0].x, this.snakeParts[0].y, 20, 20);
+
+    // Emit message to the server
+    socket.emit('snake', this.snakeParts);
+
+    socket.on('snake', snakeData => {
+      ctx.fillStyle = 'red';
+      snakeData.forEach(snakePart => {
+        ctx.fillRect(snakePart.x, snakePart.y, 20, 20);
+        ctx.strokeRect(snakePart.x, snakePart.y, 20, 20);
+      });
+    });
   }
 
   moveSnake = () => {
@@ -36,17 +47,6 @@ class Snake {
     } else if (head.y === -20) {
       head.y = 380;
     }
-
-    // Emit message to the server
-    socket.emit('snake', this.snakeParts);
-
-    socket.on('snake', snakeData => {
-      ctx.fillStyle = 'red';
-      snakeData.forEach(snakePart => {
-        ctx.fillRect(snakePart.x, snakePart.y, 20, 20);
-        ctx.strokeRect(snakePart.x, snakePart.y, 20, 20);
-      });
-    });
 
     // Snake ate food logic
     if (head.x === initialFoodX && head.y === initialFoodY) {
