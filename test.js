014c2715e590f9b25f15ea232c298efa334cd0e7
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-23 23:22:46 +0200

    Testing js logic for random numbers

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 const numbers = [];
 
+for (let i = 0; i < 100; i++) {
+  let randomNumber = Math.random() * 256;
+  randomNumber = Math.floor(randomNumber);
+  numbers.push(randomNumber);
+}
+
+console.log(numbers.sort((a, b) => a - b));
+
+// console.log(Math.floor());
+
+/*
+
 
 for (let i = 0; i < 100; i++) {
   const x = Math.floor(Math.random() * 20) * 20;
   numbers.push(x);
 }
 
-console.log(numbers.sort((a, b) => a - b));
+*/
