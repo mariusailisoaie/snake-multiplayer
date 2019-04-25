aa2c4e01260f555cf71b536eedbd08161788ffed
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2019-04-25 21:38:53 +0200

    Set PORT correctly for heroku

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.static('../public'));
 
-const PORT = 3000;
-const server = app.listen(PORT || process.env.PORT, () => {
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
 
